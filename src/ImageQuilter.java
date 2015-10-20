c4fab415390af2d407c3c8ef42d60b59729021be
damanzano <damh24@gmail.com>
2015-10-20 13:47:53 -0500

    Select the patches that satisfy threshold ssd difference

--- src/ImageQuilter.java
+++ src/ImageQuilter.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.LinkedList;
 import java.util.logging.Logger;
 
 import org.opencv.core.Core;
@@ -83,31 +84,30 @@ public class ImageQuilter {
 
 		// Create the output image
 		Mat output = new Mat(okHeight, okWidth, CvType.CV_8UC3);
-
-		// Choose a random place to get the first patch
-		int x = (int) (Math.random() * (textureImage.cols() - patchSize));
-		int y = (int) (Math.random() * (textureImage.rows() - patchSize));
+		
+		// Get the first patch to start the process
+		selectFirstRandomPatch(output);
 
 		// Save the best set of distances between patches
 		double dists[][] = new double[textureImage.cols() - patchSize][textureImage
 				.rows() - patchSize];
-
+		//
 		for (int r = 0; r < okWidth; r += patchSize - overlapSize) {
 			for (int c = 0; c < okHeight; c += patchSize - overlapSize) {
+				// Get the output cell to be analyzed
 				Mat outputCell = output.submat(new Rect(c, r, patchSize,
 						patchSize));
 
-				// Get a crop of the source texture image
-				Rect roi = new Rect(x, y, patchSize, patchSize);
-				Mat cropped = new Mat(textureImage, roi);
-
-				// Replace the output cell with the selected source patch
-				cropped.copyTo(outputCell);
-
-				// Get the patches of source texture image that satisfy the
-				// overlap constraints
+				// Get the all patches of source texture image and their differences 
 				Point bestLoc = calcDists(dists, outputCell, c, r);
 				double bestval = dists[(int)bestLoc.x][(int)bestLoc.y];
+				
+				
+				// Filter the ones that satisfy the overlap constraints
+				double threshold = bestval*1.1;
+				LinkedList<Point> loclist = getBestOverlaps(dists, threshold);
+				int choice = (int) (Math.random() * loclist.size());
+				Point loc = loclist.get(choice);
 			}
 		}
 
@@ -136,8 +136,7 @@ public class ImageQuilter {
 		// TwoDLoc bestloc = calcDists(dists, outPatch);
 		// double bestval = dists[bestloc.getRow()][bestloc.getCol()];
 		//
-		// // pick one of the close matches
-		// double threshold = bestval * 1.1;
+		//  
 		// LinkedList loclist = SynthAide.lessThanEqual(dists, threshold);
 		// int choice = (int) (Math.random() * loclist.size());
 		// TwoDLoc loc = (TwoDLoc) loclist.get(choice);
@@ -155,6 +154,28 @@ public class ImageQuilter {
 
 		return output;
 	}
+	
+	/**
+	 * This method selects a random patch from the source texture and put it in
+	 * top left corner of an output image
+	 * 
+	 * @param output
+	 *            The output image to be synthesized
+	 */
+	private void selectFirstRandomPatch(Mat output) {
+		// Choose a random place to get the first patch
+		int x = (int) (Math.random() * (textureImage.cols() - patchSize));
+		int y = (int) (Math.random() * (textureImage.rows() - patchSize));
+
+		// Get a crop of the source texture image
+		Rect roi = new Rect(x, y, patchSize, patchSize);
+		Mat cropped = new Mat(textureImage, roi);
+
+		Mat firstCell = output.submat(new Rect(0, 0, patchSize, patchSize));
+
+		// Replace the output cell with the selected source patch
+		cropped.copyTo(firstCell);
+	}
 
 	/**
 	 * This method calculates the distance (SSD) between the overlap part of
@@ -167,43 +188,44 @@ public class ImageQuilter {
 	 *            This will be filled in. The return value in dists[y][x] will
 	 *            be the SSD between an input patch with corner (x,y) and the
 	 *            given output patch.
+	 * @param outputCell
+	 * 			The output cell to be analyzed
+	 * @param cellCol
+	 * 			Current column of output cell on the whole output
+	 * @param cellRow
+	 * 			Current row of output cell on the whole output
 	 */
 	private Point calcDists(double[][] dists, Mat outputCell, int cellCol,
 			int cellRow) {
 
 		double best = Double.MAX_VALUE;
 		Point bestloc = null;
+		
 
 		// loop over the possible input patch row locations
-		for (int y = 0; y < textureImage.rows(); y += patchSize) {
-			for (int x = 0; x > textureImage.cols(); x += patchSize) {
+		for (int y = 0; y < textureImage.rows() - patchSize; y++) {
+			for (int x = 0; x > textureImage.cols() - patchSize; x++) {
 				Mat sourceCell = textureImage.submat(new Rect(x, y, patchSize,
 						patchSize));
 
 				double sum = 0.0;
-				int count = 0;
 				Mat leftOverlapDiff = null;
 				Mat topOverlapDiff = null;
 
 				// Calculate ssd of left overlap
 				if (cellCol != 0) {
-					Mat sourceLeft = sourceCell.submat(new Rect(0, 0,
-							patchSize, sourceCell.rows()));
-					Mat outputRight = outputCell.submat(new Rect(outputCell
-							.cols() - patchSize, 0, patchSize, outputCell
-							.rows()));
-					Core.subtract(outputRight, sourceLeft, leftOverlapDiff);
+					Mat sourceLeft = sourceCell.submat(new Rect(0, 0, overlapSize, sourceCell.rows()));
+					Mat outputLeft = outputCell.submat(new Rect(0, 0, overlapSize, outputCell.rows()));
+					Core.subtract(outputLeft, sourceLeft, leftOverlapDiff);
 					Core.pow(leftOverlapDiff, 2, leftOverlapDiff);
 					sum += Core.sumElems(leftOverlapDiff).val[0];
 				}
 
 				// Calculate ssd of top overlap
 				if (cellRow != 0) {
-					Mat sourceTop = sourceCell.submat(new Rect(0, 0, patchSize,
-							sourceCell.cols()));
-					Mat outputBottom = outputCell.submat(new Rect(0, outputCell
-							.rows() - patchSize, outputCell.cols(), patchSize));
-					Core.subtract(outputBottom, sourceTop, topOverlapDiff);
+					Mat sourceTop = sourceCell.submat(new Rect(0, 0, sourceCell.cols(), overlapSize));
+					Mat outputTop = outputCell.submat(new Rect(0, 0, outputCell.cols(), overlapSize));
+					Core.subtract(outputTop, sourceTop, topOverlapDiff);
 					Core.pow(topOverlapDiff, 2, topOverlapDiff);
 					sum += Core.sumElems(topOverlapDiff).val[0];
 				}
@@ -230,6 +252,26 @@ public class ImageQuilter {
 		return bestloc;
 	}
 
+	
+	/**
+	 * This method return a list of the top left points of overlaps that satisfy
+	 * the threshold difference
+	 * 
+	 * @param vals
+	 * @param threshold
+	 * @return
+	 */
+	private LinkedList<Point> getBestOverlaps(double[][] vals, double threshold){
+		LinkedList<Point> list = new LinkedList<>();
+		for(int r=0; r<vals.length; r++){
+			for(int c=0; c<vals[r].length; c++){
+				if(vals[r][c] >= 0 && vals[r][c] <=threshold){
+					list.addFirst(new Point(c, r));
+				}
+			}
+		}
+		return list;
+	}
 	public static void main(String[] args) {
 		File textureFile = new File("resources/textures/0.jpg");
 		Mat textureImage = new Mat();
